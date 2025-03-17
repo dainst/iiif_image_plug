6f43388acc2c96cdd5a6d6405ae7b522fa7d5ca6
Simon Hohl <mail@dersmon.net>
2025-03-17 21:33:59 +0100

    Use default settings in test server

--- test/support/server.ex
+++ test/support/server.ex
@@ -11,9 +11,6 @@ defmodule Server do
       scheme: :http,
       server: "localhost",
       prefix: "/",
-      max_height: 5000,
-      max_width: 5000,
-      max_area: 5000 * 5000,
       identifier_to_path_callback: &ImageStore.identifier_to_path/1,
       status_callbacks: %{
         404 => &ImageStore.handle_404/2
