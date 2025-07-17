4d5abb57063ec804e3dcff1bf77075c47870aa16
Simon Hohl <mail@dersmon.net>
2025-07-17 16:08:22 +0200

    Revert "Update docs and readmes"
    This reverts commit adabf05fedd04df6ad50ba586c79cd3de0df514a.

--- lib/iiif_image_plug/v3/options.ex
+++ lib/iiif_image_plug/v3/options.ex
@@ -4,11 +4,10 @@ defmodule IIIFImagePlug.V3.Options do
 
   The plug relies on [Vix](https://hex.pm/packages/vix) package for the image processing, which includes a
   precompiled libvips binary out of the box (Linux/MacOS). The possible values for `:preferred_formats` and
-  `:extra_formats` thus are currently as follows: `[:jpg, :png, :webp, :tif, :gif, :raw, :vips]`.
+  `extra_formats` thus are currently as follows: `[:jpg, :png, :webp, :tif, :gif, :raw, :vips]`.
 
   You can configure Vix to use your own installation of libvips if you need other formats, see the
-  [Vix documentation](https://hexdocs.pm/vix/readme.html#content), then add the format suffix to one of the
-  options.
+  [Vix documentation](https://hexdocs.pm/vix/readme.html#content).
 
   ## Options
 
@@ -19,7 +18,7 @@ defmodule IIIFImagePlug.V3.Options do
   The maximum image height the plug will serve.
 
   ### `:max_area` (default: `100000000`)
-  The maximum amount of image pixels the plug will serve (does not necessarily have to be `max_width * max_height`).
+  The maximum amount of image pixels the plug will serve (does not necessarily have to be `max_width` * `max_height`).
 
   ### `:preferred_formats` (default: `[:jpg]`)
   The [preferred formats](https://iiif.io/api/image/3.0/#55-preferred-formats) to be used for your service.
@@ -32,8 +31,8 @@ defmodule IIIFImagePlug.V3.Options do
   To be more precise, the default evaluates [System.tmp_dir!/0](https://hexdocs.pm/elixir/System.html#tmp_dir!/0) and creates
   a directory "iiif_image_plug" there.
 
-  Because of how the TIF, raw and vips file formats are structured, the plug can not stream the those if they were _requested_ as the response
-  [format](https://iiif.io/api/image/3.0/#45-format). Instead, the image gets written to a temporary file, which is then streamed
+  Because of how the TIF file format is structured, the plug can not stream the image if tif was _requested_ as the response
+  [format](https://iiif.io/api/image/3.0/#45-format). Instead, the image gets first written to a temporary file, which is then streamed
   from disk and getting deleted afterwards.
 
   If you want to forgo this file creation, you can set this option to `:buffer` instead of a file path. This will configure
