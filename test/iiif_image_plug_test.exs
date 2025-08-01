dainst/iiif_image_plug

commit 6034164b57ea6329b9a423510a32d2938c61fc6c

Remove unused fallback functions

lib/iiif_image_plug/v3/size.ex
@@ -73,10 +73,6 @@ defmodule IIIFImagePlug.V3.Size do
     end
   end
 
-  def parse(error, _params, _settings) do
-    error
-  end
-
   def apply(%Image{} = image, %Scaling{scale: 1, vscale: nil}) do
     image
   end
@@ -87,10 +83,6 @@ defmodule IIIFImagePlug.V3.Size do
 
   def apply(%Image{} = image, %Scaling{scale: scale, vscale: vscale}) do
     Operation.resize!(image, scale, vscale: vscale)
-  end
-
-  def apply(_image, _scaling) do
-    {:error, :invalid_size}
   end
 
   defp parse_percent(%Image{} = image, parameter, upscale?, %Settings{} = settings) do
@@ -287,9 +279,5 @@ defmodule IIIFImagePlug.V3.Size do
       _ ->
         {:error, :invalid_size}
     end
-  end
-
-  defp parse_w_h(_image, _w_h_parameter, _upscale?, _maintain_ratio?, _settings) do
-    {:error, :invalid_size}
   end
 end

commit 55a9b2f1d7564834bc04b8b1b070caca6c1e60e9

Handle invalid size parameters without a comma
This fixes a regression introduced with 6034164b57ea6329b9a423510a32d2938c61fc6c

lib/iiif_image_plug/v3/data/size.ex
@@ -280,4 +280,14 @@ defmodule IIIFImagePlug.V3.Data.Size do
         {:error, :invalid_size}
     end
   end
+
+  defp parse_w_h(
+         _image,
+         _not_a_list_with_two_values,
+         _upscale?,
+         _maintain_ratio?,
+         _options
+       ) do
+    {:error, :invalid_size}
+  end
 end

test/iiif_image_plug_test.exs
@@ -306,6 +306,13 @@ defmodule IIIFImagePlug.V3Test do
 
       assert conn.state == :sent
       assert conn.status == 400
+
+      conn = conn(:get, "/#{@sample_jpg_name}/full/nope/0/default.jpg")
+
+      conn = DevServerRouter.call(conn, @opts)
+
+      assert conn.state == :sent
+      assert conn.status == 400
     end
 
     test "returns 400 for invalid percent size parameter" do